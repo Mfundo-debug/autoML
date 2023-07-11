7b9c9eb6d0ab9e36f546adfdc60aa08ec9507294
Mfundo_debug <diditmfundo@gmail.com>
2023-07-11 10:16:57 +0200

    updated files

--- app.py
+++ app.py
@@ -3,21 +3,25 @@ import pandas as pd
 import os
 import pandas_profiling 
 from streamlit_pandas_profiling import st_profile_report
-from pycaret.regression import *
+from pycaret.classification import setup, compare_models, pull, save_model
+
+
+if os.path.exists('data.csv'):
+    df = pd.read_csv('data.csv', index_col=None)
 
 with st.sidebar:
     st.image('image/logo.png', width=300)
     st.title('Auto ML')
-    choice = st.radio("Navigation", ['Upload','Profiling','Modeling','Download'])
+    choice = st.radio("Navigation", ['Upload', 'Profiling', 'Modeling', 'Download'])
     st.info('This application allows you to build an automated machine learning model for your data. Please upload your data and select the target variable. The application will automatically perform data profiling, data cleaning, feature engineering, model selection, hyperparameter tuning, and model evaluation. The application will also provide you with the best model and the code to reproduce the model. The application is built using Streamlit and AutoML libraries.')
 
-if os.path.exists('data.csv'):
-    df = pd.read_csv('data.csv', index_col=None)
 if choice == 'Upload':
     st.header('Upload Data')
     st.subheader('Upload your data here')
-    st.write('Please upload your data in csv format. The data should not have any categorical variables. The data should not have any duplicate columns.')
+    st.write('Please upload your data in CSV format. The data should not have any categorical variables. The data should not have any duplicate columns.')
+
     uploaded_file = st.file_uploader("Choose a file", type=['csv'])
+    
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
         st.write(df.head())
@@ -28,9 +32,19 @@ if choice == 'Upload':
         st.write('Number of duplicate columns:', df.T.duplicated().sum())
         st.write('Number of duplicate rows:', df.duplicated().sum())
         st.write('Number of unique values in each column:', df.nunique())
-        st.write('Number of unique values in each column:', df.nunique())
         st.write('Remove duplicate columns and duplicate rows before proceeding to the next step.')
-        st.write('Removing the duplicate columns and duplicate rows...', df.drop_duplicates(inplace=True))
+
+        if st.button('Remove Duplicates'):
+            df_cleaned = df.drop_duplicates()
+            st.write('Shape of the cleaned data:', df_cleaned.shape)
+            st.write('Columns of the cleaned data:', df_cleaned.columns)
+            st.write('Data types of the cleaned data:', df_cleaned.dtypes)
+            st.write('Number of missing values in each column (cleaned data):', df_cleaned.isnull().sum())
+            st.write('Number of duplicate columns (cleaned data):', df_cleaned.T.duplicated().sum())
+            st.write('Number of duplicate rows (cleaned data):', df_cleaned.duplicated().sum())
+            st.write('Number of unique values in each column (cleaned data):', df_cleaned.nunique())
+            df = df_cleaned
+
     if st.button('Save'):
         df.to_csv('data.csv', index=False)
         st.success('Data saved successfully')
@@ -44,6 +58,7 @@ if choice == 'Modeling':
     st.title('Machine Learning')
     target = st.selectbox('Select the target variable', df.columns)
     st.write('The target variable is:', target)
+
     if st.button('Train Model'):
         st.success('Model trained successfully')
         setup(df, target=target)
@@ -51,18 +66,17 @@ if choice == 'Modeling':
         st.info('This is the ML experiment settings')
         st.dataframe(setup_df)
         best_model = compare_models()
+        compare_models_df = pull()
         st.info('This is the best model')
-        st.dataframe(best_model)
+        st.dataframe(compare_models_df)
         st.info('This is the model performance')
-        best_model
-        save_model(best_model, 'best_model')
+        st.dataframe(best_model)
+        save_model(best_model, 'best_model.pkl')
 
 if choice == 'Download':
     st.header('Download the best model')
     st.write('Please click the button below to download the best model')
+
     if st.button('Download'):
-        st.markdown(get_model('best_model'))
+        st.markdown(get_model('best_model.pkl'))
         st.success('Model downloaded successfully')
-
-
-
